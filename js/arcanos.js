be609b11d8d12a94858214d755118ffcd671cf0d
tarotia-app-psi <eduardomspati@gmail.com>
2026-08-04 16:46:54 -0300

    Add complete set of tarot cards to arcanos.js

--- js/arcanos.js
+++ js/arcanos.js
@@ -1,1 +1,23 @@
 
+// ==========================================
+// MAZO DE ARCANOS
+// ==========================================
+
+const arcanosCompleto = [
+    "El Loco", "El Mago", "La Sacerdotisa", "La Emperatriz", "El Emperador", "El Papa", 
+    "Los Enamorados", "El Carro", "La Justicia", "El Ermitaño", "La Rueda de la Fortuna", 
+    "La Fuerza", "El Colgado", "La Muerte", "La Templanza", "El Diablo", "La Torre", 
+    "La Estrella", "La Luna", "El Sol", "El Juicio", "El Mundo",
+    "As de Bastos", "2 de Bastos", "3 de Bastos", "4 de Bastos", "5 de Bastos", 
+    "6 de Bastos", "7 de Bastos", "8 de Bastos", "9 de Bastos", "10 de Bastos", 
+    "Sota de Bastos", "Caballero de Bastos", "Reina de Bastos", "Rey de Bastos",
+    "As de Copas", "2 de Copas", "3 de Copas", "4 de Copas", "5 de Copas", 
+    "6 de Copas", "7 de Copas", "8 de Copas", "9 de Copas", "10 de Copas", 
+    "Sota de Copas", "Caballero de Copas", "Reina de Copas", "Rey de Copas",
+    "As de Espadas", "2 de Espadas", "3 de Espadas", "4 de Espadas", "5 de Espadas", 
+    "6 de Espadas", "7 de Espadas", "8 de Espadas", "9 de Espadas", "10 de Espadas", 
+    "Sota de Espadas", "Caballero de Espadas", "Reina de Espadas", "Rey de Espadas",
+    "As de Oros", "2 de Oros", "3 de Oros", "4 de Oros", "5 de Oros", 
+    "6 de Oros", "7 de Oros", "8 de Oros", "9 de Oros", "10 de Oros", 
+    "Sota de Oros", "Caballero de Oros", "Reina de Oros", "Rey de Oros"
+];
